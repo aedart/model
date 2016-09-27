2f66339652ae08378bcab8f752d02ddd3a480f75
Alin Eugen Deac <aedart@gmail.com>
2016-09-27 18:10:35 +0200

    Add author and email

--- awareofcomponent.scaffold.php
+++ awareofcomponent.scaffold.php
@@ -321,6 +321,32 @@ return [
 //            }
         ],
 
+        'author' => [
+
+            'type'          => \Aedart\Scaffold\Contracts\Templates\Data\Type::QUESTION,
+
+            'question'      => 'Author name?',
+
+            'value'         => git_user_name(),
+        ],
+
+        'email' => [
+
+            'type'          => \Aedart\Scaffold\Contracts\Templates\Data\Type::QUESTION,
+
+            'question'      => 'Author email?',
+
+            'value'         => git_user_email(),
+
+            'postProcess'   => function($answer, array $previousAnswers){
+                if(!filter_var($answer, FILTER_VALIDATE_EMAIL) === false){
+                    return $answer;
+                }
+
+                throw new \InvalidArgumentException(sprintf('Email "%s" is invalid', $answer));
+            }
+        ],
+
         'castType' => [
 
             'postProcess'   => function($answer, array $previousAnswers){
