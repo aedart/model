f4368b453671323a34f9f04dbb44e6fd28d98609
Alin Eugen Deac <aedart@gmail.com>
2016-10-01 10:55:10 +0200

    Add getter setter test-case

---
+++ tests/testCases/ModelGstTestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+use Aedart\Testing\GST\GetterSetterTraitTester;
+use Aedart\Testing\TestCases\Unit\UnitTestCase;
+
+/**
+ * Model Gst Test-Case
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+abstract class ModelGstTestCase extends UnitTestCase
+{
+    use GetterSetterTraitTester;
+}
