1469fb9d73ed674df8c159892b6542b73260e16e
Alin Eugen Deac <aedart@gmail.com>
2016-10-01 14:09:27 +0200

    Add specialised aware of scaffold

---
+++ modelawareofcomponent.scaffold.php
@@ -0,0 +1,104 @@
+<?php
+
+/* ------------------------------------------------------------
+ | Require helpers
+ * ------------------------------------------------------------ */
+require __DIR__ . '/helpers/helpers.php';
+
+/* ------------------------------------------------------------
+ | Require aware of scaffold configuration
+ * ------------------------------------------------------------ */
+
+$scaffold = require __DIR__ . '/vendor/aedart/scaffold-bundle/awareofcomponent.scaffold.php';
+
+/* ------------------------------------------------------------
+ | Name
+ * ------------------------------------------------------------ */
+
+$scaffold['name'] = 'Model Aware Of Component';
+
+/* ------------------------------------------------------------
+ | Description
+ * ------------------------------------------------------------ */
+
+$scaffold['description'] = 'Specialised Aedart/Model Aware Of Component';
+
+/* ------------------------------------------------------------
+ | Base Directory
+ * ------------------------------------------------------------ */
+
+// TODO: We shouldn't need to do this, yet we are adding our own template in the mix
+// TODO: @see https://github.com/aedart/scaffold/issues/6
+$scaffold['basePath'] = __DIR__ . '/resources/';
+
+/* ------------------------------------------------------------
+ | Template Data
+ * ------------------------------------------------------------ */
+
+$scaffold['templateData']['vendorNamespace'] = [
+
+    'postProcess'   => function($answer, array $previousAnswers){
+        return 'Aedart\\Model';
+    }
+];
+
+$scaffold['templateData']['fakerGetSetValue'] = [
+
+    'postProcess'   => function($answer, array $previousAnswers){
+        $type = $previousAnswers['dataType'];
+
+        return generate_faker_method($type);
+    }
+];
+
+/* ------------------------------------------------------------
+ | Templates
+ * ------------------------------------------------------------ */
+
+$scaffold['templates']['dataProvider'] = [
+    // NOTE: Base Path is inside local vendor. We must therefore go out
+    // of it.
+    'source'        => 'snippets/provider.php.twig',
+
+    'destination'   => [
+
+        'postProcess'   => function($answer, array $previousAnswers){
+
+            $type = $previousAnswers['dataType'];
+            if(strpos($type, '[]') !== false){
+                $type = 'array';
+            }
+
+            // Group
+            switch ($type){
+                case 'int':
+                case 'integer':
+                    $type = 'integer';
+                    break;
+
+                case 'bool':
+                case 'boolean':
+                    $type = 'boolean';
+                    break;
+
+                case 'float':
+                case 'double':
+                case 'real':
+                    $type = 'float';
+                    break;
+
+                default:
+                    break;
+            }
+
+            $folder = \Illuminate\Support\Str::plural($type);
+
+            return 'tests/_data/awareOf/' . $folder . '/' . lcfirst($previousAnswers['coreProperty']) . '.php';
+        }
+    ],
+];
+
+/* ------------------------------------------------------------
+ | Finally, return the modified scaffold configuration
+ * ------------------------------------------------------------ */
+return $scaffold;
