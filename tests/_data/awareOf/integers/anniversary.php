bc76653374f884b08f7f4ab3864cf679947b8441
Alin Eugen Deac <aedart@gmail.com>
2016-12-25 16:56:10 +0100

    Fix bad generator
    Fixed #2

--- tests/_data/awareOf/integers/anniversary.php
+++ tests/_data/awareOf/integers/anniversary.php
@@ -8,12 +8,8 @@ return function(\Faker\Generator $faker){
         'int anniversary'   => [
             \Aedart\Model\Traits\Integers\AnniversaryTrait::class,
             \Aedart\Model\Contracts\Integers\AnniversaryAware::class,
-            $faker->randomNumber(time()),
-            $faker->randomNumber(time()),
-
-            // BUG: @see https://github.com/aedart/model/issues/2
-            //$faker->randomNumber($faker->unixTime),
-            //$faker->randomNumber($faker->unixTime),
+            $faker->unixTime,
+            $faker->unixTime,
         ]
     ];
 };
